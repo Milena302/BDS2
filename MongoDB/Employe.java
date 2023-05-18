326375deeda99da4f543368e1472187bea0a7580
Milena302 <76401962+Milena302@users.noreply.github.com>
2023-05-18 11:30:35 +0200

    Update Employe.java

--- MongoDB/Employe.java
+++ MongoDB/Employe.java
@@ -1,3 +1,31 @@
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase; 
+import com.mongodb.MongoClient; 
+import com.mongodb.MongoCredential; 
+import com.mongodb.DBObject;  
+import com.mongodb.BasicDBObject; 
+import com.mongodb.DBCollection; 
+import com.mongodb.DBCursor;
+import com.mongodb.DB; 
+import org.bson.Document;  
+import java.util.Arrays;
+import java.util.List;
+import com.mongodb.client.FindIterable; 
+import com.mongodb.client.ListIndexesIterable;
+import java.util.Iterator; 
+import java.util.ArrayList;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Indexes;
+import com.mongodb.client.model.IndexOptions;
+
 public class Employe{
 
+    public static void main( String args[] ){
+        System.out.println("Ca marche");
+    }
 }
